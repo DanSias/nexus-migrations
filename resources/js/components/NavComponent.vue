8478df962f032f2397335fbc20a80ce557e89b60
Daniel Sias <daniel.sias@gmail.com>
2018-12-21 23:32:16 -0500

    Primary navbar component

---
+++ resources/js/components/NavComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+            <div class="container">
+                <a class="navbar-brand" href="/">
+                    neXus
+                </a>
+                <button class="navbar-toggler" type="button">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'navbar'
+}
+</script>
