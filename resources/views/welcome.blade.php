0a251fdf29c1789d2c3a51a0420d0c4b3b9234f9
Daniel Sias <daniel.sias@gmail.com>
2018-12-21 23:32:07 -0500

    Style updates for testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,14 @@
         <title>neXus</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Orbitron:200,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <style>
             html, body {
                 background-color: #fff;
                 color: #636b6f;
-                font-family: 'Nunito', sans-serif;
+                font-family: 'Orbitron', sans-serif;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
@@ -64,7 +64,10 @@
         </style>
     </head>
     <body>
-        <span></span>
+        <div id="app">
+            <example-component></example-component>
+        </div>
+
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -86,12 +89,8 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/home">Home</a>
+                    <a href="/login">Login</a>
                 </div>
             </div>
         </div>
